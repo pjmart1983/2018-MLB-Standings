2f79b497c34c4a1fe09d962ceaf2ebf1d878336e
Peter Martin <39222489+pjmart1983@users.noreply.github.com>
2018-06-11 11:31:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const about=()=>{
-  document.getElementById("content").innerHTML="<p>aspiring web developer</p>";
+  document.getElementById("content").innerHTML="<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rutrum scelerisque semper. Mauris nunc ante, malesuada quis consequat eget, consectetur a lacus. Donec ultricies metus id maximus efficitur. Pellentesque commodo lectus non libero imperdiet, pharetra maximus felis dignissim. Duis ut placerat lacus. Proin ornare sem leo, et eleifend justo lacinia eu. Ut luctus, felis nec tincidunt finibus, nibh magna mattis sapien, et suscipit neque odio in sapien. Vestibulum vulputate massa in quam pharetra, aliquam efficitur risus mollis. Nam finibus sapien non mauris dignissim, non scelerisque metus pharetra. Quisque vulputate ornare urna.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rutrum scelerisque semper. Mauris nunc ante, malesuada quis consequat eget, consectetur a lacus. Donec ultricies metus id maximus efficitur. Pellentesque commodo lectus non libero imperdiet, pharetra maximus felis dignissim. Duis ut placerat lacus. Proin ornare sem leo, et eleifend justo lacinia eu. Ut luctus, felis nec tincidunt finibus, nibh magna mattis sapien, et suscipit neque odio in sapien. Vestibulum vulputate massa in quam pharetra, aliquam efficitur risus mollis. Nam finibus sapien non mauris dignissim, non scelerisque metus pharetra. Quisque vulputate ornare urna.</p>";
 }
 
 const portfolio=()=>{
@@ -15,3 +15,4 @@ document.getElementById("about").addEventListener("click", about);
 document.getElementById("portfolio").addEventListener("click", portfolio);
 
 document.getElementById("contact").addEventListener("click", contact);
+
